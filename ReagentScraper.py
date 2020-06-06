66f67a7d6531a6b37818c05b92284b9db26d9cf1
J-E-J-S <james.sanders1711@gmail.com>
2020-06-06 22:22:33 +0100

    comments

--- ReagentScraper.py
+++ ReagentScraper.py
@@ -105,7 +105,7 @@ def graph_format(balanced_data):
     return countries, value_import, value_export, qnt_import, qnt_export
 
 def value_graph(countries, value_import, value_export):
-
+    ''' Creates a 2-part bar graph for values of import/export by country ''' 
     divisions = countries
     imports = value_import
     exports = value_export
